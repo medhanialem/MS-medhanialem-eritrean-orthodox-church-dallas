f81302726204934cf76a951aad06242340370ee7
Samicoen <skflay@mum.edu>
2019-04-11 23:46:05 -0500

    First Commit.

---
+++ Medhanialem/src/main/java/com/Medhanialem/MedhanialemApplication.java
@@ -0,0 +1,13 @@
+package com.Medhanialem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedhanialemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedhanialemApplication.class, args);
+	}
+
+}
---
+++ Medhanialem/src/test/java/com/Medhanialem/MedhanialemApplicationTests.java
@@ -0,0 +1,16 @@
+package com.Medhanialem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MedhanialemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
