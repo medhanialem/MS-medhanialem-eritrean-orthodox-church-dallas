dcd66f3629f319f5e9a5e6d98e637f71163cfedd
filiboy03 <filiboy03@gmail.com>
2019-05-10 13:12:45 -0500

    Member CRUD oprations for MySql added

---
+++ Medhanialem/src/main/java/com/Medhanialem/controller/MemberController.java
@@ -0,0 +1,78 @@
+package com.Medhanialem.controller;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Medhanialem.exception.ResourceNotFoundException;
+import com.Medhanialem.model.Member;
+import com.Medhanialem.repository.MemberRepository;
+
+
+@RestController
+public class MemberController {
+
+	Logger logger = LoggerFactory.getLogger(MemberController.class);
+
+	@Autowired
+	MemberRepository memberRepository;
+
+	@PostMapping("/members")
+	public Member createMember(@Valid @RequestBody Member member) {
+
+		return memberRepository.save(member);
+	}
+
+	// Get All Members
+	@GetMapping("/members")
+	public List<Member> getAllMembers() {
+		return memberRepository.findAll();
+	}
+
+	// Get one Member
+	@GetMapping("/members/{id}")
+	public Member getNote(@PathVariable(value = "id") Long memberid) {
+
+		return memberRepository.findById(memberid)
+				.orElseThrow(() -> new ResourceNotFoundException("Member", "id", memberid));
+	}
+
+	// Update a Member
+	@PutMapping("/members/{id}")
+	public Member updateNote(@PathVariable(value = "id") Long memId, @Valid @RequestBody Member memberDetails) {
+
+		Member member = memberRepository.findById(memId)
+				.orElseThrow(() -> new ResourceNotFoundException("Member", "id", memId));
+
+		member.setFirstName(memberDetails.getFirstName());
+		member.setHomePhone(memberDetails.getHomePhone());
+
+		Member updatedMember = memberRepository.save(member);
+		return updatedMember;
+	}
+
+	// Delete a Member
+	@DeleteMapping("/members/{id}")
+	public ResponseEntity<?> deleteNote(@PathVariable(value = "id") Long memId) {
+		Member note = memberRepository.findById(memId)
+				.orElseThrow(() -> new ResourceNotFoundException("Member", "id", memId));
+
+		memberRepository.delete(note);
+
+		return ResponseEntity.ok().build();
+	}
+
+}
