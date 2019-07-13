a17d0b6edac5adfcd8f857e050dd1d5f5ca36be6
filiboy03 <filiboy03@gmail.com>
2019-07-12 23:13:25 -0500

    repository membership Fee lookup

--- Medhanialem/src/main/java/com/Medhanialem/controller/PaymentJournalcontroller.java
+++ Medhanialem/src/main/java/com/Medhanialem/controller/PaymentJournalcontroller.java
@@ -1,32 +1,47 @@
-
 package com.Medhanialem.controller;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
+import org.joda.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.Medhanialem.model.Member;
 import com.Medhanialem.model.payment.MembershipPaymentLookupFee;
+import com.Medhanialem.repository.MemberRepository;
 import com.Medhanialem.repository.MemberShipPaymentLookUpfeeRepository;
+
 
 @RestController
 public class PaymentJournalcontroller {
 
-//	@Autowired
-//	MemberRepository memberRepository;
+	@Autowired
+	MemberRepository memberRepository;
 
 	@Autowired
 	MemberShipPaymentLookUpfeeRepository memberShipPaymentLookUpfee;
 
-	@GetMapping("/getpaymentinfo")
-	public List<MembershipPaymentLookupFee> getMember() {
+	@GetMapping("/getpaymentinfo/{id}")
+	public List<MembershipPaymentLookupFee> getPaymentInformation(@PathVariable(value = "id") Long id) {
 
-		// Optional<Member> member = memberRepository.findById(id);
+		 Member member = memberRepository.findById(id).get();
 
 		// memberShipPaymentLookUpfee.findAll();
+		 
+		 Long lookUpId = member.getMembershippaymentlookupfee().getId();
+		 
+		 
+		int year = LocalDate.now().getYear();
+		 
+		int month = LocalDate.now().getMonthOfYear();
+		 
+		 
 
-		return memberShipPaymentLookUpfee.findpaylookUpfees();
+		return memberShipPaymentLookUpfee.findpaylookUpfees(lookUpId,year,month);
 
 	}
 
--- Medhanialem/src/main/java/com/Medhanialem/repository/MemberShipPaymentLookUpfeeRepository.java
+++ Medhanialem/src/main/java/com/Medhanialem/repository/MemberShipPaymentLookUpfeeRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.Medhanialem.model.payment.MembershipPaymentLookupFee;
@@ -12,8 +13,8 @@ import com.Medhanialem.model.payment.MembershipPaymentLookupFee;
 @Repository
 public interface MemberShipPaymentLookUpfeeRepository extends JpaRepository<MembershipPaymentLookupFee,Long> {
 	
-	@Query(value = "SELECT * FROM medhanie_alem_dallas.membership_payment_lookup_fee WHERE `id` > 6 and year <= 2019 and month <= 7", 
+	@Query(value = "SELECT * FROM medhanie_alem_dallas.membership_payment_lookup_fee WHERE `id` > :id  and year <= :year and month <= :month", 
 			  nativeQuery = true)
-	List<MembershipPaymentLookupFee> findpaylookUpfees();
+	List<MembershipPaymentLookupFee> findpaylookUpfees(@Param(value = "id") Long id,@Param(value = "year") int year,@Param(value = "month") int month);
 
 }
