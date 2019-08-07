efcbbced3879dbd139169c03044640fff39405c7
Daniel Kifle <kifledaniel7@gmail.com>
2019-08-06 23:57:58 -0500

    Jwt code changes

---
+++ Medhanialem/src/main/java/com/Medhanialem/jwtauthentication/request/Signup.java
@@ -0,0 +1,45 @@
+package com.Medhanialem.jwtauthentication.request;
+
+
+
+import java.util.Set;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Signup {
+    @NotBlank
+    @Size(min=3, max = 60)
+    private String username;
+
+    @NotBlank
+    @Size(min = 6, max = 40)
+    private String password;
+
+    private Set<String> role;
+    
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+	public Set<String> getRole() {
+		return role;
+	}
+
+	public void setRole(Set<String> role) {
+		this.role = role;
+	}
+    
+}
