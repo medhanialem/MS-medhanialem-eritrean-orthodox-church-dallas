5380ba49d9ffa9887c346cb8f55cb6295a580aa1
Samicoen <skflay@mum.edu>
2019-09-21 20:50:12 -0500

    Payment Lookup API by year updated.

--- Medhanialem/src/main/java/com/Medhanialem/controller/PaymentJournalcontroller.java
+++ Medhanialem/src/main/java/com/Medhanialem/controller/PaymentJournalcontroller.java
@@ -14,11 +14,13 @@ import org.springframework.web.bind.annotation.RestController;
 import com.Medhanialem.controller.model.payment.MonthlyPaid;
 import com.Medhanialem.controller.model.payment.PaymentInformation;
 import com.Medhanialem.controller.model.payment.PaymentLogs;
+import com.Medhanialem.controller.model.payment.PaymentLookUps;
+import com.Medhanialem.controller.model.payment.PaymentLookupResponseByYear;
 import com.Medhanialem.controller.model.payment.Paymentrequest;
 import com.Medhanialem.model.Member;
-import com.Medhanialem.model.payment.PaymentLookup;
 import com.Medhanialem.model.payment.Payment;
 import com.Medhanialem.model.payment.PaymentJournal;
+import com.Medhanialem.model.payment.PaymentLookup;
 import com.Medhanialem.model.payment.PaymentlogDTO;
 import com.Medhanialem.repository.MemberRepository;
 import com.Medhanialem.repository.MemberShipPaymentLookUpfeeRepository;
@@ -31,7 +33,7 @@ public class PaymentJournalcontroller {
 
 	@Autowired
 	MemberRepository memberRepository;
-	
+
 	@Autowired
 	PaymentlogRepositoryjdbc paymentlogRepositoryjdbc;
 
@@ -45,59 +47,74 @@ public class PaymentJournalcontroller {
 	MemberShipPaymentLookUpfeeRepository memberShipPaymentLookUpfee;
 
 	@GetMapping("/getpaymentlookupinfo/{year}")
-	public List<PaymentLookup> getPaymentLookupInfo(@PathVariable(value = "year") Long year) {
-		return memberShipPaymentLookUpfee.getPaymentLookupInfo(year);
-	}
+	public List<PaymentLookupResponseByYear> getPaymentLookupInfo(@PathVariable(value = "year") Long year) {
+		List<PaymentLookup> mplfList = memberShipPaymentLookUpfee.getPaymentLookupInfo(year);
+		List<PaymentLookupResponseByYear> plrbyList = new ArrayList<>();
 
+		for (int i = 1; i < 5; i++) {
+			List<PaymentLookUps> plList = new ArrayList<>();
+			PaymentLookupResponseByYear paymentLookUpResponseByYear = new PaymentLookupResponseByYear();
+			for (PaymentLookup memberLookUpfee : mplfList) {
+
+				if (memberLookUpfee.getTier().getId() == i) {
+					PaymentLookUps paymentLookUp = new PaymentLookUps();
+					paymentLookUp.setPaymentLookupId(memberLookUpfee.getId());
+					paymentLookUp.setMonth(memberLookUpfee.getMonth());
+					paymentLookUp.setYear(memberLookUpfee.getYear());
+					paymentLookUp.setAmount(memberLookUpfee.getAmount());
+					plList.add(paymentLookUp);
+
+				}
+			}
+			paymentLookUpResponseByYear.setTierId((long) i);
+			paymentLookUpResponseByYear.setPaymentLookUps(plList);
+			plrbyList.add(paymentLookUpResponseByYear);
+		}
+		return plrbyList;
+	}
 
 	// This API will be used to post payment to DB
 
 	@PostMapping("/pay")
-	public void paymonthlyfee( @RequestBody Paymentrequest paymentRequest) {
-		
+	public void paymonthlyfee(@RequestBody Paymentrequest paymentRequest) {
+
 		Member member = memberRepository.findById(paymentRequest.getMemberId()).get();
 
 		Payment payment = new Payment();
 		payment.setMember(member);
 		payment.setTotal(paymentRequest.getTotal());
-	
+
 		payment = paymentRepository.save(payment);
 
 		for (MonthlyPaid monthlypaid : paymentRequest.getPayments()) {
-			
 
 			PaymentJournal paymentJournal = new PaymentJournal();
 
-		
 			paymentJournal.setAmount(monthlypaid.getAmount());
 
-			
 			paymentJournal.setPayment(payment);
 			paymentJournal.setMember(member);
-			paymentJournal.setPaymentLookupfee(memberShipPaymentLookUpfee.findById(monthlypaid.getPaymentLookupId()).get());
+			paymentJournal
+					.setPaymentLookupfee(memberShipPaymentLookUpfee.findById(monthlypaid.getPaymentLookupId()).get());
 
 			paymentJournalRepository.save(paymentJournal);
 		}
 	}
 
-
-	
-   //This method fetches payment information for all members for a selected year
+	// This method fetches payment information for all members for a selected year
 	@GetMapping("/getallpayment/{year}")
 	public List<PaymentInformation> getAllPayment(@PathVariable(value = "year") int year) {
 
-		
-		 List<Member> memberList = memberRepository.findAll().parallelStream().filter(t -> t.getSuperId() == 1)
-		  .collect(Collectors.toList());
+		List<Member> memberList = memberRepository.findAll().parallelStream().filter(t -> t.getSuperId() == 1)
+				.collect(Collectors.toList());
 
-		List<PaymentInformation> list  = new ArrayList<>();
+		List<PaymentInformation> list = new ArrayList<>();
 
-	
-		List<PaymentlogDTO> listPaymentdto = paymentlogRepositoryjdbc.findAll( year);
-		
+		List<PaymentlogDTO> listPaymentdto = paymentlogRepositoryjdbc.findAll(year);
+
 		for (Member member : memberList) {
 			PaymentInformation response = new PaymentInformation();
-			
+
 			response.setMemberId(member.getId());
 			response.setChurchId(member.getChurchId());
 			response.setFirstName(member.getFirstName());
@@ -108,7 +125,7 @@ public class PaymentJournalcontroller {
 			response.setTier(member.getTier().getId());
 			response.setRegistrationDate(member.getRegistrationDate());
 
-			List<PaymentLogs> journals = getPaymentLogs(listPaymentdto,member);
+			List<PaymentLogs> journals = getPaymentLogs(listPaymentdto, member);
 			response.setPaymentLogs(journals);
 
 			list.add(response);
@@ -118,23 +135,23 @@ public class PaymentJournalcontroller {
 		return list;
 	}
 
-	private List<PaymentLogs> getPaymentLogs(List<PaymentlogDTO> listPaymentdto,Member member ) {
+	private List<PaymentLogs> getPaymentLogs(List<PaymentlogDTO> listPaymentdto, Member member) {
 
 		List<PaymentLogs> paymentLoglist = new ArrayList<>();
-		
+
 		for (PaymentlogDTO paymentdto : listPaymentdto) {
-			
-			if(paymentdto.getMemberId().equals(member.getId())) {
 
-			PaymentLogs paylog = new PaymentLogs();
+			if (paymentdto.getMemberId().equals(member.getId())) {
 
-			paylog.setPaymentLogId(paymentdto.getPaymentLogId());
+				PaymentLogs paylog = new PaymentLogs();
 
-			paylog.setMonth(paymentdto.getMonth());
-			paylog.setYear(paymentdto.getYear());
-			paylog.setAmount(paymentdto.getAmount());
+				paylog.setPaymentLogId(paymentdto.getPaymentLogId());
 
-			paymentLoglist.add(paylog);
+				paylog.setMonth(paymentdto.getMonth());
+				paylog.setYear(paymentdto.getYear());
+				paylog.setAmount(paymentdto.getAmount());
+
+				paymentLoglist.add(paylog);
 			}
 		}
 
