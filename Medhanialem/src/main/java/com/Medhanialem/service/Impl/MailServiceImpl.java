9d369258765d8ca939602b58b63e78856824e2a1
medhanialem <medhanialemdallas@gmail.com>
2020-09-19 18:57:02 -0500

    removed unneeded imports

--- Medhanialem/src/main/java/com/Medhanialem/service/Impl/MailServiceImpl.java
+++ Medhanialem/src/main/java/com/Medhanialem/service/Impl/MailServiceImpl.java
@@ -9,8 +9,6 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Service;
 
 import com.Medhanialem.service.MailService;
